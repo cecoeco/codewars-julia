1305ff186edd279630f5bb79e3d016a56bc9ef6b
Ceco E Maples <108555136+cecoeco@users.noreply.github.com>
2023-10-21 16:43:03 -0400

    Update Vowel Count.jl

--- 7 kyu/Vowel Count.jl
+++ 7 kyu/Vowel Count.jl
@@ -1,9 +1,3 @@
 function getcount(s::String)
-    vowels = 0
-    for char in s
-        if char in "aeiou"
-            vowels += 1
-        end
-    end
-    return vowels
+  count(c -> c in "aeiou", s)
 end
