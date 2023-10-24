927dee06a0c0f6a6e2c0d0edcbaac344afc000a8
Ceco E Maples <108555136+cecoeco@users.noreply.github.com>
2023-10-24 19:51:16 -0400

    Create Who likes it?.jl

---
+++ 6 kyu/Who likes it?.jl
@@ -0,0 +1,18 @@
+module Kata
+  export likes
+  
+  function likes(names::Vector{String}) :: String
+    n = length(names)
+    if n == 0
+        return "no one likes this"
+    elseif n == 1
+        return names[1] * " likes this"
+    elseif n == 2
+        return join(names, " and ") * " like this"
+    elseif n == 3
+        return join(names[1:2], ", ") * " and " * names[3] * " like this"
+    else
+        return join(names[1:2], ", ") * " and $(n - 2) others like this"
+    end
+  end
+end
