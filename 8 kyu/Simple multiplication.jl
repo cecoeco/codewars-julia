03aa46787be940b09d61bdb8e5d9f6f66eaaa888
Ceco E Maples <108555136+cecoeco@users.noreply.github.com>
2023-10-23 22:20:06 -0400

    Create Simple multiplication.jl

---
+++ 8 kyu/Simple multiplication.jl
@@ -0,0 +1,11 @@
+module Solution
+  export simpleMultiplication
+
+  function simpleMultiplication(number)
+    if isodd(number)
+      return number * 9
+    else
+      return number * 8
+    end
+  end
+end
