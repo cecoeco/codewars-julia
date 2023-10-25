02465db78dabca94a6174f23f56a19461ed3dee5
Ceco E Maples <108555136+cecoeco@users.noreply.github.com>
2023-10-25 16:24:44 -0400

    Update and rename .jl to Sum of Intervals.jl

---
+++ 4 kyu/Sum of Intervals.jl
@@ -0,0 +1,23 @@
+function sumofintervals(a)
+    # Sort intervals by their start points
+    sorted_intervals = sort(a, by=x->x[1])
+    
+    total_length = 0
+    current_start = sorted_intervals[1][1]
+    current_end = sorted_intervals[1][2]
+    
+    for interval in sorted_intervals[2:end]
+        if interval[1] <= current_end  # Check for overlap
+            current_end = max(current_end, interval[2])
+        else
+            total_length += current_end - current_start
+            current_start = interval[1]
+            current_end = interval[2]
+        end
+    end
+    
+    # Add the length of the last interval
+    total_length += current_end - current_start
+    
+    return total_length
+end
