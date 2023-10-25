2ee878cf7d5fe7ea745e5c2c2507a52138f81aaf
Ceco E Maples <108555136+cecoeco@users.noreply.github.com>
2023-10-25 16:00:45 -0400

    Create Human Readable Time.jl

---
+++ 5 kyu/Human Readable Time.jl
@@ -0,0 +1,8 @@
+using Printf
+
+function humanreadable(seconds)
+    hours = seconds ÷ 3600
+    minutes = (seconds % 3600) ÷ 60
+    seconds = seconds % 60
+    return @sprintf("%02d:%02d:%02d", hours, minutes, seconds)
+end
