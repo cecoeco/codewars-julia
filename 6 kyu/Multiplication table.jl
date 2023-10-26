cfa6771016d3a2b04c636d56e335fee9273b13b8
Ceco E Maples <108555136+cecoeco@users.noreply.github.com>
2023-10-26 13:46:35 -0400

    Create Multiplication table.jl

---
+++ 6 kyu/Multiplication table.jl
@@ -0,0 +1,11 @@
+using LinearAlgebra
+
+function multiplicationtable(size::Int64)::Matrix{Int64}
+    table = zeros(Int64, size, size)
+    for i in 1:size
+        for j in 1:size
+            table[i, j] = i * j
+        end
+    end
+    return table
+end
