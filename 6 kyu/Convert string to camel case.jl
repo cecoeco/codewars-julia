5141f9467f5087b09d108a3bc0172a05b1a4d723
Ceco E Maples <108555136+cecoeco@users.noreply.github.com>
2023-10-26 14:12:45 -0400

    Create Convert string to camel case.jl

---
+++ 6 kyu/Convert string to camel case.jl
@@ -0,0 +1,11 @@
+function tocamelcase(s::AbstractString)
+    words = split(s, r"[_-]")
+    if isempty(words)
+        return ""
+    end
+
+    first_word = words[1]
+    rest_words = [uppercasefirst(word) for word in words[2:end]]
+
+    return first_word * join(rest_words)
+end
