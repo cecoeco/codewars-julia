2f7b7be72bc80c83479e0de626943c855859a07a
Ceco E Maples <108555136+cecoeco@users.noreply.github.com>
2023-10-28 12:47:14 -0400

    Create I love you, a little , a lot, passionately ... not at all.jl

---
+++ 8 kyu/I love you, a little , a lot, passionately ... not at all.jl
@@ -0,0 +1,15 @@
+function how_much_i_love_you(nbpetals)
+    if nbpetals % 6 == 1
+        return "I love you"
+    elseif nbpetals % 6 == 2
+        return "a little"
+    elseif nbpetals % 6 == 3
+        return "a lot"
+    elseif nbpetals % 6 == 4
+        return "passionately"
+    elseif nbpetals % 6 == 5
+        return "madly"
+    else
+        return "not at all"
+    end
+end
