09fa9aafa50a7c82276ce23dcf33d43e8e0ffdb3
Ceco E Maples <108555136+cecoeco@users.noreply.github.com>
2023-10-29 14:09:03 -0400

    Create Total amount of points.jl

---
+++ 8 kyu/Total amount of points.jl
@@ -0,0 +1,8 @@
+function points(games)
+    total_points = 0
+    for game in games
+        x, y = parse.(Int, split(game, ":"))
+        total_points += (x > y) * 3 + (x == y)
+    end
+    return total_points
+end
