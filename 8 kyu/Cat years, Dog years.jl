32a155edd92162ef875e1113b379874f567a62ca
cecoeco <108555136+cecoeco@users.noreply.github.com>
2023-10-30 15:26:24 -0400

    Create Cat years, Dog years.jl

---
+++ 8 kyu/Cat years, Dog years.jl
@@ -0,0 +1,13 @@
+function human_years_cat_years_dog_years(human_years) 
+    if human_years == 1
+        cat_years = 15
+        dog_years = 15
+    elseif human_years == 2
+        cat_years = 15 + 9
+        dog_years = 15 + 9
+    else
+        cat_years = 24 + 4 * (human_years - 2)
+        dog_years = 24 + 5 * (human_years - 2)
+    end
+    return [human_years, cat_years, dog_years]
+end
