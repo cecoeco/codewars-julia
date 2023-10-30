b35a1c6a8bb93922d9eb0086104dd09131778db8
cecoeco <108555136+cecoeco@users.noreply.github.com>
2023-10-30 15:20:17 -0400

    Create Student's Final Grade.jl

---
+++ 8 kyu/Student's Final Grade.jl
@@ -0,0 +1,10 @@
+    if exam > 90 || projects > 10
+        return 100
+    elseif exam > 75 && projects >= 5
+        return 90
+    elseif exam > 50 && projects >= 2
+        return 75
+    else
+        return 0
+    end
+end
