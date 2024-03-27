2fb59c9a652fc0149137cca7808640905cfa4ef7
Ceco Maples <108555136+cecoeco@users.noreply.github.com>
2024-03-27 01:51:45 -0400

    Update Correct the mistakes of the character recognition software.jl

--- 8 kyu/Correct the mistakes of the character recognition software.jl
+++ 8 kyu/Correct the mistakes of the character recognition software.jl
@@ -1,16 +1,5 @@
 # v1.5
 function correct(string)
-    corrected_string = ""
-    for char in string
-        if char == '5'
-            corrected_string *= 'S'
-        elseif char == '0'
-            corrected_string *= 'O'
-        elseif char == '1'
-            corrected_string *= 'I'
-        else
-            corrected_string *= char
-        end
-    end
-    return corrected_string
+    corrections = Dict('5' => 'S', '0' => 'O', '1' => 'I')
+    return join([get(corrections, char, char) for char in string])
 end
